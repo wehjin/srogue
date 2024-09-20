771b30de2ba64f46d67af815b8c274ed2c2c3de6
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-09-20 11:55:01 -0700

    Introduces max_depth.

--- src/resources/game.rs
+++ src/resources/game.rs
@@ -1,14 +1,19 @@
+use crate::player::LAST_DUNGEON;
 use crate::resources::dungeon::party;
 use crate::resources::level::roll_level;
 use crate::resources::level::RoomSizing;
 
 pub fn run() {
 	let mut party_depth = party::roll_next_depth(0);
-	let rogue_depth: usize = 1;
+	let mut depth: usize = 0;
+	let mut max_depth: usize = 0;
 	for _ in 0..1 {
-		let room_sizing = if rogue_depth == party_depth { RoomSizing::BigRoll } else { RoomSizing::SmallAlways };
-		let level = roll_level(rogue_depth, room_sizing);
-
+		if depth < LAST_DUNGEON as usize {
+			depth += 1;
+			max_depth = max_depth.max(depth);
+		}
+		let room_sizing = if depth == party_depth { RoomSizing::BigRoll } else { RoomSizing::SmallAlways };
+		let level = roll_level(depth, room_sizing);
 		level.map.print();
 
 		if level.depth == party_depth {
