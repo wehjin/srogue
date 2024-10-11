a182b2a6987f95d0bf3a64e239d0883dacb7b6ff
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-10-10 17:31:40 -0700

    Converts mv_one_mon into an event.

--- src/resources/play/event/game/mod.rs
+++ src/resources/play/event/game/mod.rs
@@ -3,7 +3,7 @@ use crate::actions::quaff::QuaffPotionEvent;
 use crate::resources::play::context::RunContext;
 use crate::resources::play::event::check_hunger::CheckHungerEvent;
 use crate::resources::play::event::game::drop_item::DropItemEvent;
-use crate::resources::play::event::move_monsters::MoveMonstersEvent;
+use crate::resources::play::event::move_monsters::{MoveMonsterFullEvent, MoveMonstersEvent};
 use crate::resources::play::event::one_move::OneMoveEvent;
 use crate::resources::play::event::reg_move::RegMoveEvent;
 use crate::resources::play::event::upgrade_rogue::UpgradeRogueEvent;
@@ -16,6 +16,7 @@ pub enum GameEvent {
 	RegMove(RegMoveEvent),
 	OneMove(OneMoveEvent),
 	MoveMonsters(MoveMonstersEvent),
+	MoveMonsterFull(MoveMonsterFullEvent),
 	CheckHunger(CheckHungerEvent),
 	DropItem(DropItemEvent),
 	EatMeal(EatMealEvent),
@@ -28,6 +29,7 @@ impl Dispatch for GameEvent {
 			Self::CheckHunger(event) => event.dispatch(state, ctx),
 			Self::DropItem(event) => event.dispatch(state, ctx),
 			Self::MoveMonsters(event) => event.dispatch(state, ctx),
+			Self::MoveMonsterFull(event) => event.dispatch(state, ctx),
 			Self::OneMove(event) => event.dispatch(state, ctx),
 			Self::RegMove(event) => event.dispatch(state, ctx),
 			Self::EatMeal(event) => event.dispatch(state, ctx),
--- src/resources/play/event/move_monsters.rs
+++ src/resources/play/event/move_monsters.rs
@@ -1,5 +1,6 @@
 use crate::init::Dungeon;
 use crate::monster;
+use crate::monster::{mon_can_go_and_reach, move_confused};
 use crate::resources::arena::Arena;
 use crate::resources::avatar::Avatar;
 use crate::resources::play::context::RunContext;
@@ -37,19 +38,21 @@ impl Dispatch for MoveMonstersEvent {
 				false => match mon_ids.pop() {
 					None => after_move.into_step(state),
 					Some(mon_id) => {
-						let state = match state.as_monster(mon_id).is_defeated() {
-							true => state,
-							false => mv_one_mon(mon_id, state, ctx),
-						};
-						Self { mon_ids: Some(mon_ids), after_move }.into_redirect(state)
+						match state.as_monster(mon_id).is_defeated() {
+							true => Self { mon_ids: Some(mon_ids), after_move }.into_redirect(state),
+							false => {
+								let after_mon_move = |state| Self { mon_ids: Some(mon_ids), after_move }.into_redirect(state);
+								MoveMonsterFullEvent::new(mon_id, after_mon_move).into_redirect(state)
+							}
+						}
 					}
 				},
-			},
+			}
 		}
 	}
 }
 
-pub fn get_monster_ids_for_movement(state: &RunState) -> Vec<u64> {
+fn get_monster_ids_for_movement(state: &RunState) -> Vec<u64> {
 	if state.cleaned_up().is_some() || state.as_health().haste_self.is_half_active() {
 		vec![]
 	} else {
@@ -57,41 +60,86 @@ pub fn get_monster_ids_for_movement(state: &RunState) -> Vec<u64> {
 	}
 }
 
-pub fn mv_one_mon(mon_id: u64, mut game: RunState, ctx: &mut RunContext) -> RunState {
-	// TODO Make this into an event so that mv_monster can become an event. MoveEnhancedMonster.
-	let mut done_with_monster = false;
-	if game.as_monster(mon_id).is_hasted() {
-		let rogue_row = game.rogue_row();
-		let rogue_col = game.rogue_col();
-		game = monster::mv_monster(game, mon_id, rogue_row, rogue_col, false, ctx);
-		if game.get_monster(mon_id).is_none() {
-			done_with_monster = true;
-		}
-	} else if game.as_monster(mon_id).is_slowed() {
-		game.as_monster_mut(mon_id).flip_slowed_toggle();
-		if game.as_monster(mon_id).is_slowed_toggle() {
-			done_with_monster = true;
-		}
+impl GameEventVariant for MoveMonsterFullEvent {
+	fn into_game_event(self) -> GameEvent {
+		GameEvent::MoveMonsterFull(self)
 	}
-	if !done_with_monster && game.as_monster(mon_id).is_confused() {
-		if monster::move_confused(mon_id, false, &mut game) {
-			done_with_monster = true;
-		}
+}
+#[derive(Debug)]
+pub enum MoveMonsterFullEvent {
+	Start { mon_id: u64, after_move: StepSeed },
+	Step { count: usize, mon_id: u64, after_move: StepSeed },
+	ConfusedStep { count: usize, mon_id: u64, after_move: StepSeed },
+	AirStep { count: usize, mon_id: u64, after_move: StepSeed },
+	GroundStep { count: usize, mon_id: u64, after_move: StepSeed },
+	End { after_move: StepSeed },
+}
+impl MoveMonsterFullEvent {
+	pub fn new(mon_id: u64, after_move: impl FnOnce(RunState) -> RunStep + 'static) -> Self {
+		Self::Start { mon_id, after_move: StepSeed::new("move-monster-ful", after_move) }
 	}
-	if !done_with_monster {
-		let mut flew = false;
-		let rogue_row = game.rogue_row();
-		let rogue_col = game.rogue_col();
+}
 
-		if game.as_monster(mon_id).flies()
-			&& !game.as_monster(mon_id).is_napping()
-			&& !monster::mon_can_go_and_reach(mon_id, rogue_row, rogue_col, false, &game) {
-			flew = true;
-			game = monster::mv_monster(game, mon_id, rogue_row, rogue_col, false, ctx);
-		}
-		if !(flew && monster::mon_can_go_and_reach(mon_id, rogue_row, rogue_col, false, &game)) {
-			game = monster::mv_monster(game, mon_id, rogue_row, rogue_col, false, ctx);
+impl Dispatch for MoveMonsterFullEvent {
+	fn dispatch(self, mut state: RunState, ctx: &mut RunContext) -> RunStep {
+		match self {
+			Self::Start { mon_id, after_move } => {
+				let count = if state.as_monster(mon_id).is_hasted() {
+					2
+				} else if state.as_monster(mon_id).is_slowed() {
+					state.as_monster_mut(mon_id).flip_slowed_toggle();
+					if state.as_monster(mon_id).is_slowed_toggle() { 0 } else { 1 }
+				} else {
+					1
+				};
+				Self::Step { count, mon_id, after_move }.into_redirect(state)
+			}
+			Self::Step { count, mon_id, after_move } => {
+				if count == 0 {
+					Self::End { after_move }.into_redirect(state)
+				} else {
+					Self::ConfusedStep { count, mon_id, after_move }.into_redirect(state)
+				}
+			}
+			Self::ConfusedStep { count, mon_id, after_move } => {
+				if state.as_monster(mon_id).is_confused() && move_confused(mon_id, false, &mut state) {
+					Self::Step { count: count - 1, mon_id, after_move }.into_redirect(state)
+				} else {
+					Self::AirStep { count, mon_id, after_move }.into_redirect(state)
+				}
+			}
+			Self::AirStep { count, mon_id, after_move } => {
+				let rogue_row = state.rogue_row();
+				let rogue_col = state.rogue_col();
+				let flies = state.as_monster(mon_id).flies();
+				let not_napping = !state.as_monster(mon_id).is_napping();
+				let cant_reach_rogue = !mon_can_go_and_reach(mon_id, rogue_row, rogue_col, false, &state);
+				if flies && not_napping && cant_reach_rogue {
+					// If monster flies and cannot reach the rogue in one step, then the monster is allowed
+					// to attack from two steps away.
+					state = monster::mv_monster(state, mon_id, rogue_row, rogue_col, false, ctx);
+					let still_cant_reach_rogue = !mon_can_go_and_reach(mon_id, rogue_row, rogue_col, false, &state);
+					if still_cant_reach_rogue {
+						Self::Step { count: count - 1, mon_id, after_move }.into_redirect(state)
+					} else {
+						Self::GroundStep { count, mon_id, after_move }.into_redirect(state)
+					}
+				} else {
+					Self::GroundStep { count, mon_id, after_move }.into_redirect(state)
+				}
+			}
+			Self::GroundStep { count, mon_id, after_move } => {
+				let rogue_row = state.rogue_row();
+				let rogue_col = state.rogue_col();
+				// TODO Convert mv_monster into an event.
+				let state = monster::mv_monster(state, mon_id, rogue_row, rogue_col, false, ctx);
+				let new_count = match state.get_monster(mon_id) {
+					None => 0,
+					Some(monster) => if monster.is_defeated() { 0 } else { count - 1 }
+				};
+				Self::Step { count: new_count, mon_id, after_move }.into_redirect(state)
+			}
+			Self::End { after_move } => after_move.into_step(state),
 		}
 	}
-	game
 }
